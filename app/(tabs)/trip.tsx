36e98997b69d923182b8befe382407a6a09ad842
Ahmedhamdy640 <ah812185@gmial.com>
2025-04-21 20:44:57 +0200

    fix trip header

--- app/(tabs)/trip.tsx
+++ app/(tabs)/trip.tsx
@@ -88,7 +88,6 @@ const Trip = () => {
                   marginLeft: 15,
                   backgroundColor: "#fff",
                   borderRadius: 25,
-                  // marginRight: 15,
                   padding: 8,
                 }}
                 onPress={handleBack}
